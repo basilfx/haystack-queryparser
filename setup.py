85f6fa8529b88e159870bee87b76187ad3b6a4ac
Vignesh Sarma K <vignesh.sarma@gmail.com>
2012-12-24 10:31:27 +0530

    A simple setup file, Intent to have full distutil suport.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='haystack_queryparser',
+      version='0.1',
+      description='A search query parser that works in conjunction with haystack',
+      author='Vignesh Sarma K',
+      author_email='vignesh@recruiterbox.com',
+      url='https://github.com/recruiterbox/haystack-queryparser',
+      py_modules=['haystack_queryparser'],
+      )

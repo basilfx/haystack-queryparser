9bc2f2b0297d406530e9995a0ca37b7acbcffa25
Vignesh Sarma K <vignesh.sarma@gmail.com>
2013-01-06 22:57:58 +0530

    small changes to better support python distribution.

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from getSQ import ParseSQ
-from getSQ import NoMatchingBracketsFound
-from getSQ import UnhandledException
-
-or_parser = ParseSQ('OR')
-and_parser = ParseSQ('AND')
--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 from distutils.core import setup
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(name='haystack_queryparser',
       version='0.1',
       description='A search query parser that works in conjunction with haystack',
+      long_description = long_description,
       author='Vignesh Sarma K',
       author_email='vignesh@recruiterbox.com',
       url='https://github.com/recruiterbox/haystack-queryparser',
-      py_modules=['haystack_queryparser'],
+      classifiers = [
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Development Status :: 5 - Production/Stable",
+          "Intended Audience :: Developers",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+      ],
+      keywords = ["parsing", "query", "search"],
+      packages = ['haystack_queryparser'],
       )
